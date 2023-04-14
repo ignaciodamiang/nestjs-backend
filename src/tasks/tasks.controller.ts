c27acf04f84117ec1304a6fb1d732f8113cc05e5
Ignacio Damián González <igonzalez@startuniversity.net>
2023-04-14 20:54:23 -0300

    crear y listar

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { TasksService } from './tasks.service';
+import { CreateTaskDto } from './dto/task.dto';
 
 @Controller('tasks')
 export class TasksController {
+  private _tasksService: TasksService;
+  constructor(_tasksService: TasksService) {
+    this._tasksService = _tasksService;
+  }
+
   @Get()
-  helloWorld() {
-    return 'Hello World!';
+  getAllTasks() {
+    return this._tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body() newTask: CreateTaskDto) {
+    return this._tasksService.createTask(newTask.title, newTask.description);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,4 +1,45 @@
 import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.entity';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  private tasks: Task[] = [
+    {
+      id: '1',
+      title: 'Task 1',
+      description: 'This is task 1',
+      status: TaskStatus.PENDING,
+    },
+    {
+      id: '2',
+      title: 'Task 2',
+      description: 'This is task 2',
+      status: TaskStatus.IN_PROGRESS,
+    },
+    {
+      id: '3',
+      title: 'Task 3',
+      description: 'This is task 3',
+      status: TaskStatus.DONE,
+    },
+  ];
+
+  getAllTasks() {
+    return this.tasks;
+  }
+
+  createTask(title: string, description: string) {
+    const task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.PENDING,
+    };
+    this.tasks.push(task);
+
+    return task;
+  }
+  updateTask() {}
+  deleteTask() {}
+}
