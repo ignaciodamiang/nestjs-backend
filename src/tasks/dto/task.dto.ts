a9115cbc378ece63de05ccaad30e322cd7377b78
Ignacio Damián González <igonzalez@startuniversity.net>
2023-04-14 22:27:48 -0300

    validations

--- src/tasks/dto/task.dto.ts
+++ src/tasks/dto/task.dto.ts
@@ -1,12 +1,37 @@
 import { TaskStatus } from '../task.entity';
+import {
+  IsIn,
+  IsNotEmpty,
+  IsOptional,
+  IsString,
+  MinLength,
+} from 'class-validator';
 
 export class CreateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   title: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
   description: string;
 }
 
 export class UpdateTaskDto {
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsOptional()
   title?: string;
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(3)
+  @IsOptional()
   description?: string;
+  @IsString()
+  @IsNotEmpty()
+  @IsOptional()
+  @IsIn([TaskStatus.PENDING, TaskStatus.IN_PROGRESS, TaskStatus.DONE])
   status?: TaskStatus;
 }
